aa359ae31b25cfc55bb10b1986d99b34e2d24203
Lance Fowler <lancefowler22@gmail.com>
2021-05-24 21:15:45 -0400

    pretty much works

--- SquareGame.py
+++ SquareGame.py
@@ -9,7 +9,7 @@ roomSize = 700
 class Box:
 	def __init__(self):
 		self.x = 15
-		self.y = 15
+		self.y = 685
 		self.dy = 0
 		self.dx = 0
 		self.g = -0.1
@@ -18,10 +18,10 @@ class Box:
 		self.width = 10
 		self.height = 10
 		self.onGround = False
-		self.box = room.create_rectangle(10, 10, 10 + self.width, 10 + self.height, fill='blue')
+		self.box = room.create_rectangle(10, 690, 10 + self.width, 690 - self.height, fill='blue')
 		
 		self.platforms = [Platform(130, 670, 170, 680), Platform(0, 640, 100, 660), Platform(200, 640, 250, 700), Platform(250, 570, 280, 700), Platform(320, 520, 340, 630), DisappearingPlatform(600, 670, 700, 680, 3, 1, 0)]
-		self.movingPlatforms = [MovingPlatform(40, 600, 90, 610, 0, 10, 0.2)]
+		self.movingPlatforms = [MovingPlatform(40, 600, 90, 610, 110, 0, 1)]
 
 		room.after(10, self.loop)
 		
@@ -65,7 +65,7 @@ class Box:
 			
 	def boundingPlatforms(self):
 		for platform in self.platforms:
-			move = platform.bounding(self.x, self.y, self.width, self.height, self.dx, self.dy, 0, 0)
+			move = platform.bounding(self.x, self.y, self.width, self.height, self.dx, self.dy)
 			self.x += move[0]
 			self.y += move[1]
 			room.move(self.box, move[0], move[1])
@@ -77,7 +77,7 @@ class Box:
 	def boundingMovingPlatforms(self):
 		for platform in self.movingPlatforms:
 			platform.slide()
-			move = platform.bounding(self.x, self.y, self.width, self.height, self.dx, self.dy, platform.xSpeed * (1 if platform.forward else -1), platform.ySpeed * (1 if platform.forward else -1))
+			move = platform.bounding(self.x, self.y, self.width, self.height, self.dx, self.dy)
 			self.x += move[0]
 			self.y += move[1]
 			room.move(self.box, move[0], move[1])
@@ -102,24 +102,24 @@ class Platform:
 		self.width = abs(x1 - x2)
 		self.platform = room.create_rectangle(x1, y1, x2, y2, fill='black')
 		
-	def bounding(self, x, y, w, h, dx, dy, pdx, pdy):
+	def bounding(self, x, y, w, h, dx, dy, pdx = 0, pdy = 0):
 		xStep = 0
 		yStep = 0
 		resetDy = False
 		ground = False
 		if x + (w / 2) > self.left and x - (w / 2) < self.right:
-			if y + (h / 2) > self.top and y + (h / 2) - dy <= self.top:
-				yStep = self.top - (y + (h / 2)) + pdy
+			if y + (h / 2) > self.top and y + (h / 2) - dy <= self.top - pdy:
+				yStep = self.top - (y + (h / 2))
 				resetDy = True
 				ground = True
-			elif y - (h / 2) < self.bottom and y - (h / 2) - dy >= self.bottom:
-				yStep = self.bottom - (y - (h / 2)) + pdy
-				resetDy = False
+			elif y - (h / 2) < self.bottom and y - (h / 2) - dy >= self.bottom - pdy:
+				yStep = self.bottom - (y - (h / 2))
+				resetDy = True
 		if y - (h / 2) < self.bottom and y + (h / 2) > self.top:
-			if x - (w / 2) < self.right and x - (w / 2) - dx >= self.right:
-				xStep = self.right - (x - (w / 2)) + pdx
-			elif x + (w / 2) > self.left and x + (w / 2) - dx <= self.left:
-				xStep = self.left - (x + (w / 2)) + pdx
+			if x - (w / 2) < self.right and x - (w / 2) - dx >= self.right - pdx:
+				xStep = self.right - (x - (w / 2))
+			elif x + (w / 2) > self.left and x + (w / 2) - dx <= self.left - pdx:
+				xStep = self.left - (x + (w / 2))
 		return (xStep, yStep, resetDy, ground)
 		
 	def move(self, x, y):
@@ -150,12 +150,14 @@ class MovingPlatform(Platform):
 		self.ySpeed = yDelta * scale
 		self.xDelta = xDelta
 		self.yDelta = yDelta
+		self.pdx = 0
+		self.pdy = 0
 		self.x = 0
 		self.y = 0
 		self.forward = True
 		
-	def bounding(self, x, y, w, h, dx, dy, pdx, pdy):
-		output = super().bounding(x, y, w, h, dx, dy, pdx, pdy)
+	def bounding(self, x, y, w, h, dx, dy):
+		output = super().bounding(x, y, w, h, dx, dy, self.xSpeed * (1 if self.forward else -1), 0)
 		xStep = output[0]
 		yStep = output[1]
 		resetDy = output[2]
@@ -166,11 +168,11 @@ class MovingPlatform(Platform):
 		return (xStep, yStep, resetDy, ground)
 		
 	def slide(self):
-		dx = self.xSpeed * (1 if self.forward else -1)
-		dy = self.ySpeed * (1 if self.forward else -1)
-		super().move(dx, dy)
-		self.x += dx
-		self.y += dy
+		self.pdx = self.xSpeed * (1 if self.forward else -1)
+		self.pdy = self.ySpeed * (1 if self.forward else -1)
+		super().move(self.pdx, self.pdy)
+		self.x += self.pdx
+		self.y += self.pdy
 		if (self.x >= self.xDelta and self.y >= self.yDelta) or (self.x <= 0 and self.y <= 0):
 			self.forward = not self.forward
 
@@ -183,7 +185,7 @@ class DisappearingPlatform(Platform):
 		self.time = tShift * 100
 		super().changeColor('green')
 	
-	def bounding(self, x, y, w, h, dx, dy, pdx, pdy):
+	def bounding(self, x, y, w, h, dx, dy):
 		self.time += 1
 		if self.time < self.tOn:
 			color = "#"
@@ -197,7 +199,7 @@ class DisappearingPlatform(Platform):
 			self.time %= self.tOff + self.tOn
 
 		if self.time < self.tOn:
-			return super().bounding(x, y, w, h, dx, dy, pdx, pdy)
+			return super().bounding(x, y, w, h, dx, dy)
 		return (0, 0, False, False)
 
 
